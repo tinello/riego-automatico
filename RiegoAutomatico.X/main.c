aeddbead6af6c3b2d329f11ec2a34c1830b5aa47
Gustavo <gustinello@yahoo.com.ar>
2022-10-02 19:20:42 -0300

    Start Project

---
+++ RiegoAutomatico.X/main.c
@@ -0,0 +1,41 @@
+/*
+ * File:   main.c
+ * Author: gustavo
+ *
+ * Created on 2 de octubre de 2022, 15:48
+ */
+
+
+// PIC16F819 Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG
+#pragma config FOSC = INTOSCIO  // Oscillator Selection bits (INTRC oscillator; port I/O function on both RA6/OSC2/CLKO pin and RA7/OSC1/CLKI pin)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = ON       // RA5/MCLR/VPP Pin Function Select bit (RA5/MCLR/VPP pin function is MCLR)
+#pragma config BOREN = ON       // Brown-out Reset Enable bit (BOR enabled)
+#pragma config LVP = ON         // Low-Voltage Programming Enable bit (RB3/PGM pin has PGM function, Low-Voltage Programming enabled)
+#pragma config CPD = OFF        // Data EE Memory Code Protection bit (Code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off)
+#pragma config CCPMX = RB2      // CCP1 Pin Selection bit (CCP1 function on RB2)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#include <xc.h>
+
+
+void main(void) {
+    
+    int x = 0;
+    
+    while (x == 0) {
+        _delay(1000);
+        x = x + 1;
+    }
+    
+    return;
+}
